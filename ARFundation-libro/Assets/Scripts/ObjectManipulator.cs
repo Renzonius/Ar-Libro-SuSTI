2bdb4af4b7458594c0480fbc1dca827d1d83e3fe
Renzonius <48992705+Renzonius@users.noreply.github.com>
2023-10-05 09:05:30 -0300

    Arrancamos

---
+++ ARFundation-libro/Assets/Scripts/ObjectManipulator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManipulator : MonoBehaviour
+{
+    public GameObject ARObject;
+    [SerializeField] private Camera 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
